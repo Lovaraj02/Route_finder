c690f8cee3664c6d6b644f612e844efeab4d0296
raju <rajubandaru2255@gmail.com>
2024-10-11 10:50:51 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// App.js starts here
+
+import logo from './logo.svg';
+import './App.css';
+import Mycomponent from './MyComponent';
+
+
+function App() {
+  return (
+    <>
+    <Mycomponent/>
+    
+      
+    </>
+  );
+}
+
+export default App;
+
